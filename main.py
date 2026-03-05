badd2ab68f37debe4c41af7abdef99fa1b340584
Oswaldo Ibarra Cárdenas <1224100506@alumnos.utng.edu.mx>
2026-03-04 19:19:49 -0600

    Primer commit: Integración Flask, Spotify y Supabase

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return "Home"
+
+"""
+GET    -> obtener informacion
+POST   -> Crear informacion
+PUT    -> Actualizar informacion
+DELETE -> Borrar informacion
+"""
+
+@app.route("/users/<user_id>")
+def get_user(user_id):
+    user = {'id':user_id, 'name':"test", 'telefono':123456789}
+    query = request.args.get('query')
+    if query:
+        user['query'] = query
+    return jsonify(user), 200
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    data['status'] = 'created'
+    return jsonify(data), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
